a9665673b854e781cf6964695e9df8a0c818991b
ajifirlana <32819502+Ajifirlana@users.noreply.github.com>
2024-02-10 21:12:42 +0700

    update

--- db_biodata.sql
+++ db_biodata.sql
@@ -1,33 +1,25 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 06, 2024 at 04:19 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `db_biodata`
---
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.21-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             11.2.0.6213
 -- --------------------------------------------------------
 
---
--- Table structure for table `mahasiswa`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `mahasiswa` (
-  `id` int(50) NOT NULL,
+
+-- Dumping database structure for db_biodata
+CREATE DATABASE IF NOT EXISTS `db_biodata` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `db_biodata`;
+
+-- Dumping structure for table db_biodata.mahasiswa
+CREATE TABLE IF NOT EXISTS `mahasiswa` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
   `nama` varchar(15) NOT NULL,
   `ktp` varchar(20) NOT NULL,
   `ttl` varchar(20) NOT NULL,
@@ -38,67 +30,34 @@ CREATE TABLE `mahasiswa` (
   `kecamatan` varchar(50) NOT NULL,
   `foto_ktp` varchar(50) NOT NULL,
   `foto_kk` varchar(50) NOT NULL,
-  `foto_sktm` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `foto_sktm` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `mahasiswa`
---
+-- Dumping data for table db_biodata.mahasiswa: ~1 rows (approximately)
+/*!40000 ALTER TABLE `mahasiswa` DISABLE KEYS */;
+REPLACE INTO `mahasiswa` (`id`, `nama`, `ktp`, `ttl`, `jenis_kelamin`, `no_hp`, `alamat`, `pekerjaan`, `kecamatan`, `foto_ktp`, `foto_kk`, `foto_sktm`) VALUES
+	(1, 'aji firlana', '3473276', 'JAMBI,27-MEI-1990', 'Lak-Laki', '0895777', 'JAMBI', 'KERJA', 'Muara Tembesi', '', '', ''),
+	(3, 'tes', '7476647', 'JAMBI', 'LAKI LAKI', '0847445', 'MUARA TEMBESI', 'KERJA', 'MUARA TEMBESI', '', '', '');
+/*!40000 ALTER TABLE `mahasiswa` ENABLE KEYS */;
 
-INSERT INTO `mahasiswa` (`id`, `nama`, `ktp`, `ttl`, `jenis_kelamin`, `no_hp`, `alamat`, `pekerjaan`, `kecamatan`, `foto_ktp`, `foto_kk`, `foto_sktm`) VALUES
-(1, 'aji firlana', '3473276', 'JAMBI,27-MEI-1990', 'Lak-Laki', '0895777', 'JAMBI', 'KERJA', 'Muara Tembesi', '', '', '');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(5) NOT NULL,
+-- Dumping structure for table db_biodata.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
   `email` varchar(20) NOT NULL,
-  `password` varchar(15) NOT NULL
-  `role` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` varchar(15) NOT NULL,
+  `role` enum('user','admin','','') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
+-- Dumping data for table db_biodata.users: ~2 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+REPLACE INTO `users` (`id`, `email`, `password`, `role`) VALUES
+	(1, 'firlana89@gmail.com', '123', 'user'),
+	(2, 'admin@gmail.com', '12345', 'admin');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
-INSERT INTO `users` (`id`, `email`, `password`, `role`) VALUES
-(1, 'firlana89@gmail.com', '123', 'user'),
-(2, 'admin@gmail.com', '123', 'admin');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
